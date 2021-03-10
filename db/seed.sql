9b4993cb40f303c85e896e8431bd1f7641080b72
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-10 21:41:47 +1100

    db schema seed added

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Big Mac", FALSE),
+("Double Quarter Pounder", FALSE),
+("Pulled Pork Burger", FALSE),
+("Chicken Burger", FALSE),
+("Amazing Aloha Burger", FALSE);
