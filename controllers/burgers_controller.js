0c0dc0852b6a7d225db46404d2fb2b91b9e6967b
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-10 22:33:28 +1100

    started controller setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const burger = require('../models/burger');
+
+//Create the router for the app
+
+
+//export the router at the end of your file
+module.exports = router;
