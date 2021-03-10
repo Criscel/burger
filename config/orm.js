61c9247e5786278b2698c95d55102ea1bf9d799e
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-10 22:21:23 +1100

    started orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,15 @@
 const connection = require('./connection.js');
 
+const orm = {
+    selectAll: (table, cb) => {
+      const query = 'SELECT * FROM ' + table + ';';
+      connection.query(query, [table], (err, res) => {
+        if (err) throw err;
+
+        console.log(res);
+        cb(res);
+      });
+    },
+}
+
+module.exports = orm;
