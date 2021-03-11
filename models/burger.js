0009e3be62b1b3fa2a517c49707bc3f8413ee6bf
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-11 14:46:41 +1100

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,11 +3,18 @@ const orm = require('../config/orm');
 //create the code that will call the ORM functions using burger specific input for the ORM.
 
 const burger = {
-    selectAll: (cb) => {
-        orm.selectAll('burgers', (res) => {
-            cb(res);
-        })
-    }
-}
+    all(cb) {
+      orm.all('burgers', (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    create(cols, vals, cb) {
+      orm.create('burgers', cols, vals, (res) => cb(res));
+    },
+  
+    update(objColVals, condition, cb) {
+      orm.update('burgers', objColVals, condition, (res) => cb(res));
+    },
+  };
+  
 
 module.exports = burger;
